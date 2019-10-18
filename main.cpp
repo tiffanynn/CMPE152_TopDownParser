bc584db5651f65713ef42b7460ca44572a53a9bb
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-10-17 20:58:22 -0700

    added parse tree into it

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "tree.h"
 using namespace std;
 
 int main()
@@ -8,5 +9,8 @@ int main()
 	// lex.getNextToken();
     // lex.print();
     
+    cout << "Parse tree:" << endl;
+
+
     return 0;
 }
