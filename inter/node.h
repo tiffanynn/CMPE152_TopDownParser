c5f33cc437fa44bdfe8846015c76437a79284773
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-10-25 21:33:25 -0700

    needs to be looked at

--- inter/node.h
+++ inter/node.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <vector>
-#include "lexer.h"
+#include "../lexer/lexer.h"
 
 class Node
 {	
 public:
 	Node();
+	int lexline;
 	vector<Node> children;
 	vector<Node> get_children();
 };
 
 Node::Node()
 {
+ lexline= line; //error here, cannot identify line from the lexer header file
 
  }
 
