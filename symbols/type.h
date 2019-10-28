13c525733fca663641e704aec4d2d6b0dd492b0b
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-10-27 23:44:17 -0700

    fixed

--- symbols/type.h
+++ symbols/type.h
@@ -1,55 +1,76 @@
-#include "../lexer/token.h"
+#pragma once
+#include "../lexer/word.h"
 #include "string.h"
 using namespace std;
 
 
-class Type:public Word{
-    public:
-    int width = 0;
-    Type::Type(){}
-    Type(string s, int tag, int w){
-    this->s = tag; //idk what this is
-    width = w; 
-    }
-   static Type Int()
-    {
-        Type comp;
-        comp.value = "int";
-        comp.tokenTag = "BASE_TYPE";
-        return comp;
-    };
-   
-    bool numeric(Type p);
-    Type max(Type p1, Type p2);
+class Type :public Word
+{
+public:
+	int width; //used for storage allocation, but i don't wanna worry about this
+	Type();
+	Type(string, int, int);
+	Tag t;
 
+	//use the get fucntions to acess reserved types
 
+	static Type* get_Int() {
+		static Type* Int = new Type("int", 257, 4);
+		return Int;
+	}
+	static Type* get_Char()
+	{
+		static Type *Char = new Type("char", 257, 1);
+		return Char;
+	}
+	static Type* get_Bool()
+	{
+		static Type* Bool = new Type("bool", 257, 1);
+		return Bool;
+	}
+	static Type* get_Float()
+	{
+		static Type* Float = new Type("float", 257, 8);
+		return Float;
+	}
 
+	bool isitnumeric(Type*);
+	Type* max(Type*, Type*);
+	bool reserved;
 };
-
-bool Type::numeric(Type p){
-    if(p == Type.Char || p == Type.Int){
-        return true;
-    }
-    else if(p == Type.Float){
-        return true;
-    }
-    else{
-        return false;
-    }
-    return 0;
+Type::Type(string s, int tag, int w)
+{
+	this->lexeme = s;
+	this->tag = tag;
+	reserved = false;
+	//Int = new Type("int", 278, 4);
+	width = w;
 }
 
-Type Type::max(Type p1, Type p2){
-    if(!numeric(p1)|| !numeric(p2)){
-        return NULL;
-    }
-    else if(p1 == Type.Float || p2 == Type.Int){
-        return Type.Float;
-    }
-    else if(p1 == Type.Int || p2 == Type.Int){
-        return Type.Int;
-    }
-    else{
-        return Type.Char;
-    }
+
+
+bool Type::isitnumeric(Type* p)
+{
+	//if(!reserved)
+		//reserve_default_types();
+	if (p == get_Char() || p == get_Int() || p == get_Float())
+		return true;
+	else 
+		return false;
+}
+
+Type* Type::max(Type* p1, Type* p2)
+{
+	if (!isitnumeric(p1) || !isitnumeric(p2))
+		return NULL;
+	else if (p1 == get_Float() || p2 == get_Float()) {
+	
+		return get_Float();
+	}
+	else if (p1 == get_Int() || p2 == get_Int())
+	{
+		return get_Int();
+	}
+	else return get_Char();
+
 }
