8ae909dc986c65ac8de74463d9ccc386bb4de9ed
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2019-10-28 00:35:42 -0700

    needs fixing

--- inter/rel.h
+++ inter/rel.h
@@ -10,3 +10,13 @@ class Rel: public Logical {
     Type check(Type p1, Type p2);
 };
 
+Type Rel::check(Type p1, Type p2){
+    if(p1 == p2){ //needs overloaded operator? idk
+        return this->Type.Bool();
+    }
+    else
+    {
+        return NULL;
+    }
+    return 0;
+}
